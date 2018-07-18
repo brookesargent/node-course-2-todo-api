3d642bfd5f8795503b2dac31574034be3b64c3bb
brookesargent <brookesargent01@gmail.com>
2018-07-18 09:59:36 -0400

    add server.js

---
+++ server/server.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }, 
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    completedAt: {
+        type: Number,
+        default: null
+    }
+});
+
+// var newTodo = new Todo({
+//     text: 'Cook dinner'
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log('Saved todo', doc);
+// }, (e) => {
+//     console.log('Unable to save todo');
+// });
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
+});
+
+var newUser = new User({
+    email: 'brookesargent01@gmail.com'
+});
+
+newUser.save().then((doc) => {
+    console.log('Saved user', doc);
+}, (e) => {
+    console.log('Unable to save user');
+});
